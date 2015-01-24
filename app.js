80352220b73a0150be9f1a93f8cdca2015d38592
Andrew Fyfe <enumera2000@yahoo.co.uk>
2015-01-24 15:26:46 +0000

    First commit

---
+++ app.js
@@ -0,0 +1,38 @@
+(function() {  
+
+function shuffle(o){ //v1.0
+    for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+};
+
+  var app = angular.module('SapHana', []);  
+
+    app.controller('SapHanaController', ['$http' , '$scope', '$interval', function($http, $scope, $interval){  
+
+    $scope.flickrPhotos = [];
+    $scope.flickrPhotoIds = [];
+    $scope.flickrPhotoInfo = "";
+    $scope.flickrPhotoUrls = [];
+    $scope.flickrPhotosx = [];
+    $scope.url = "";
+    $scope.categories = ['Current layers','Topogrpahy', 'Potential area', 'Heat Map', 'Ports', 'Plankton Type A', 'Plankton Type B']
+    // $scope.musicGenre = ['skiiing', 'winter%20sport'];
+    // $scope.locationWord = "in%20London";
+
+   
+    // console.log($scope.musicGenre)
+
+
+
+
+ 
+      
+  }]);
+
+})();
+
+
+
+
+
+
